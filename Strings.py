d5aa5cde2941e46277ba8b6bf33568d161e5868f
George The Dumb <engrfahimulislam@gmail.com>
2026-04-03 18:02:58 +0600

    By the user
    In this code, i learned strings in python.

---
+++ Strings.py
@@ -0,0 +1,14 @@
+# This Are Strings.
+
+
+first_name = "Finn Mertens"
+email = "youremail@gmail.com"
+food = "Pizza"
+soft_drink = "Monster"
+address = "North Carolina"
+
+print(f"hello {first_name} it's really nice too meet you.")
+print(f"Is {email} your personal email?")
+print(f"I heard you like {food} really much.")
+print(f"Is {soft_drink} your favorite drink? i like it too!")
+print(f"Whats your address? oh {address}? is your new permanent? I have friends from there too!")
