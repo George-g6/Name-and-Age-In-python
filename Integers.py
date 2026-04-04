c680435820106d78ededec590f55016be009f2d8
George The Dumb <engrfahimulislam@gmail.com>
2026-04-04 21:22:43 +0600

    By the user

---
+++ Integers.py
@@ -0,0 +1,12 @@
+# This are integers.
+
+
+age = 17
+quantity = 8
+num_of_student = 15
+day = 28
+
+print(f"Your are {age} years old.")
+print(f"You are buying total {quantity} eggs.")
+print(f"In our class there are total {num_of_student} students.")
+print(f"This year, in february it'll be {day} days in month.")
